a24354530d945c0b9b1c41478b181439e6349cac
Xzonn <Xzonn@outlook.com>
2025-08-14 23:48:25 +0800

    Update build script

--- scripts/build_patch.ps1
+++ scripts/build_patch.ps1
@@ -25,6 +25,11 @@ New-Item -ItemType Directory -Path "temp\out\data\data\dwc\" -Force | Out-Null
 Copy-Item -Path "files\dwc\utility.bin" -Destination "temp\out\data\dwc\utility.bin" -Recurse -Force
 Copy-Item -Path "files\dwc\utility.bin" -Destination "temp\out\data\data\dwc\utility.bin" -Recurse -Force
 
+New-Item -ItemType Directory -Path "out\" -Force | Out-Null
+New-Item -ItemType Directory -Path "out\preprocessing\" -Force | Out-Null
+Copy-Item -Path "files\md5.txt" -Destination "out\md5.txt" -Recurse -Force
+Copy-Item -Path "files\preprocessing\*" -Destination "out\preprocessing" -Recurse -Force
+
 python scripts\edit_banner.py
 
 $versions = @("normal", "dlp")
@@ -48,6 +53,6 @@ foreach ($version in $versions) {
 
   python scripts\create_xdelta.py
 
-  Compress-Archive -Path "out/xdelta/", "out/banner.bin" -Destination "patch-ds.zip" -Force
+  Compress-Archive -Path "out/xdelta/", "out/preprocessing/", "out/banner.bin", "out/md5.txt" -Destination "patch-ds.zip" -Force
   Move-Item -Path "patch-ds.zip" -Destination "out/patch-ds-$version.xzp" -Force
 }
